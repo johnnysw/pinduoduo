0a8de7ccd27247380981c800a1d261faf6827b54
taishuang4687 <18845772690@163.com>
2019-07-03 23:19:34 +0800

    页面

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,34 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+		<view class='shadow'></view><!-- 上部阴影部分 -->
+		<view class='cart'>
+			<view class='image'>
+				<img src="../../static/cate1.jpg" alt="">
+				<span>￥350</span><br>
+				<span>请选择： 颜色分类  尺寸</span>
+			</view>
+		<view class='size'>
+			<span>尺码</span>
+			<button class="cu-btn">S</button>
+			<button class="cu-btn">M</button>
+			<button class="cu-btn">L</button>
+			<button class="cu-btn">XL</button>
 		</view>
+		<view class='color'>
+			<span>颜色分类</span>
+			<button class="cu-btn">卡其</button>
+			<button class="cu-btn">白色</button>
+			<button class="cu-btn">黑色</button>
+			<button class="cu-btn">藏蓝</button>
+		</view>	
+		<view class='bottom'>
+			<span>购买数量</span>
+			<button class="cu-btn">-</button>
+			<button class="cu-btn">数量</button>
+			<button class="cu-btn">+</button>
+			<span class='pay'>确认</span>
+		</view>
+		</view><!-- 购物车部分 -->
 	</view>
 </template>
 
@@ -11,7 +36,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
 			}
 		},
 		onLoad() {
@@ -24,19 +49,15 @@
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
+	.content{
+		width: 100%;
+		height: 100%;
+		position: relative;
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
+	.shadow{
+		width: 100%;
+		height: 366px;
+		background: rgba(11,6,6,0.8);
 	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+	
 </style>
