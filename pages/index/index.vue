805637c1a69910e0ee9436c96b880acb380adfe9
刘园215 <1803656569@qq.com>
2019-07-04 14:25:22 +0800

    拼单

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,34 +1,10 @@
 <template>
 	<view class="content">
-		<view class='shadow'></view><!-- 上部阴影部分 -->
-		<view class='cart'>
-			<view class='image'>
-				<img src="../../static/cate1.jpg" alt="">
-				<span>￥350</span><br>
-				<span>请选择： 颜色分类  尺寸</span>
-			</view>
-		<view class='size'>
-			<span>尺码</span>
-			<button class="cu-btn">S</button>
-			<button class="cu-btn">M</button>
-			<button class="cu-btn">L</button>
-			<button class="cu-btn">XL</button>
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
 		</view>
-		<view class='color'>
-			<span>颜色分类</span>
-			<button class="cu-btn">卡其</button>
-			<button class="cu-btn">白色</button>
-			<button class="cu-btn">黑色</button>
-			<button class="cu-btn">藏蓝</button>
-		</view>	
-		<view class='bottom'>
-			<span>购买数量</span>
-			<button class="cu-btn">-</button>
-			<button class="cu-btn">数量</button>
-			<button class="cu-btn">+</button>
-			<span class='pay'>确认</span>
-		</view>
-		</view><!-- 购物车部分 -->
+		<navigator url="../collageSheet/collageSheet">跳转</navigator>
 	</view>
 </template>
 
@@ -36,7 +12,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello',
+				title: 'Hello'
 			}
 		},
 		onLoad() {
@@ -49,15 +25,19 @@
 </script>
 
 <style>
-	.content{
-		width: 100%;
-		height: 100%;
-		position: relative;
+	.content {
+		text-align: center;
+		height: 400upx;
 	}
-	.shadow{
-		width: 100%;
-		height: 366px;
-		background: rgba(11,6,6,0.8);
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
 	}
-	
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
 </style>
