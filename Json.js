b8bc96ef345cbbd0456d31cf06d42c4cda95b313
zx93623 <787807448@qq.com>
2019-07-05 17:54:50 +0800

    '团购商品列表'

---
+++ Json.js
@@ -0,0 +1,44 @@
+/* 商品列表 */
+const goodsList = [{
+		image: "./../../static/img/pic1.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 20,
+		id:1,
+	},
+	{
+		image: "./../../static/img/pic2.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 16,
+		id:2,
+	},
+	{
+		image: "./../../static/img/pic1.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 20,
+		id:3,
+	}, {
+		image: "./../../static/img/pic2.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 20,
+		id:4,
+	}, {
+		image: "./../../static/img/pic1.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 20,
+		id:5,
+	}, {
+		image: "./../../static/img/pic2.png",
+		title: "2019夏装新款欧美风女装白色",
+		price: 450,
+		sales: 20,
+		id:6,
+	},
+]
+export default {
+	goodsList
+}
--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 
+import Json from './Json'
+
+const json = type=>{
+	//模拟异步请求数据
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(Json[type]);
+		}, 500)
+	})
+}
 Vue.config.productionTip = false
+Vue.prototype.$api = {json};
 
 App.mpType = 'app'
 
