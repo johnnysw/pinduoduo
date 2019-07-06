e4596588eb50d22d66c94cbfc85bf12676ecf6ac
taishuang4687 <18845772690@163.com>
2019-07-06 23:21:58 +0800

    购物车

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,29 +1,30 @@
 <template>
 	<view class='shadow'>
-	<view class='cart'>
-		<view class='header'>
-			<img src="../../static/cate1.jpg" alt="">
-			<view class='text'>
-				<view>￥{{this.price}}</view>
-				<text>请选择：颜色分类 尺寸</text>
+		<view class='cart'>
+			<view class='header'>
+				<img src="../../static/cate1.jpg" alt="">
+				<view class='text'>
+					<view>￥{{this.price}}</view>
+					<text>请选择:颜色分类 尺寸</text>
+				</view>
 			</view>
-		</view>
-		<view class='content'>
-			<view class='list' v-for='(obj,index) in bigList' :key='index'>
-				<view class='select'>{{obj.name}}</view>
-				<view class='kind' v-for='(kindObj,index) in obj.kind' :key='index'>{{kindObj.size}}</view>
-			</view>
-			<view class='buy'>
-				<text class='select'>购买数量</text>
-				<view class='btn'>
-					<view class='kind' @click='reduce()'>-</view>
-					<view class='kind'>{{this.shuliang}}</view>
-					<view class='kind' @click='add()'>+</view>
+			<view class='content'>
+				<view class='list' v-for='(item,index) in bigList' :key='index'>
+					<view class='select'>{{item.name}}</view>
+					<view class='kind' :class='{selected: childItem.selected}' v-for='(childItem, childIndex) in smallList' v-if="childItem.pid === item.id"
+					 @click='change(childIndex, childItem.pid)' :key='childIndex'>{{childItem.name}}</view>
+				</view>
+				<view class='buy'>
+					<text class='select'>购买数量</text>
+					<view class='btn'>
+						<view class='kind reduce' @click='reduce()'>-</view>
+						<view class='kind'>{{this.shuliang}}</view>
+						<view class='kind add' @click='add()'>+</view>
+					</view>
 				</view>
 			</view>
 		</view>
 		<view class='footer'>确定</view>
-	</view>
 	</view>
 </template>
 
@@ -31,49 +32,92 @@
 	export default {
 		data() {
 			return {
-				shuliang:1,
-				price:350,
-				bigList:[{
-					name:'尺码',
-					kind:[{
-						size:'S'
-					},{
-						size:'M'
-					},{
-						size:'L'
-					},{
-						size:'XL'
-					}]
-				},{
-					name:'颜色分类',
-					kind:[{
-						size:'卡其'
-					},{
-						size:'白色'
-					},{
-						size:'黑色'
-					},{
-						size:'藏蓝'
-					}]
-				}]
+				specSelected: [],
+				shuliang: 1,
+				price: 350,
+				bigList: [
+					{
+						id: 1,
+						name: '尺寸',
+					},
+					{	
+						id: 2,
+						name: '颜色分类',
+					},
+				],
+				smallList: [
+					{
+						id: 1,
+						pid: 1,
+						name: 'S',
+					},
+					{
+						id: 2,
+						pid: 1,
+						name: 'M',
+					},
+					{
+						id: 3,
+						pid: 1,
+						name: 'L',
+					},
+					{
+						id: 4,
+						pid: 1,
+						name: 'XL',
+					},
+					{
+						id: 5,
+						pid: 2,
+						name: '白色',
+					},
+					{
+						id: 6,
+						pid: 2,
+						name: '黑色',
+					},
+					{
+						id: 7,
+						pid: 2,
+						name: '藏蓝',
+					},
+				]
+
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-			add(){
+			add() {
 				this.shuliang++;
 			},
-			reduce(){
+			reduce() {
 				this.shuliang--;
+			},
+			change(index, pid){
+				let list = this.smallList;
+				list.forEach(item=>{
+					if(item.pid === pid){
+						this.$set(item, 'selected', false);
+					}
+				})
+			
+				this.$set(list[index], 'selected', true);
+				this.specSelected = []; 
+				list.forEach(item=>{ 
+					if(item.selected === true){ 
+						this.specSelected.push(item); 
+					} 
+				})
+				
 			}
 		}
 	}
 </script>
 
 <style>
-	.shadow{
+	.shadow {
 		position: fixed;
 		top: 0;
 		width: 100%;
@@ -81,77 +125,106 @@
 		z-index: 1;
 		background-color: rgba(0, 0, 0, 0.4);
 	}
-	.cart{
+
+	.cart {
 		width: 100%;
-		height: 620upx;
+		height: 520upx;
 		position: fixed;
-		bottom:0;
+		bottom: 100upx;
 		background: #fff;
-		
+		box-sizing: border-box;
+		padding: 0 20upx;
 	}
-	.header{
+
+	.header {
 		width: 100%;
 		height: 200upx;
-		position:sticky;
-	    top:0;
-		border:0.5upx solid #ddd;
+		position: sticky;
+		top: 0;
+		border-bottom: 0.5upx solid #ddd;
 	}
-	.header img{
+
+	.header img {
 		width: 160upx;
 		height: 160upx;
+		margin-top: 20upx;
 	}
-	.text{
+
+	.text {
 		position: absolute;
-		top:100upx;
-		left:200upx;
-		color:#999;
-		font-size:20upx;
+		top: 100upx;
+		left: 180upx;
+		color: #999;
+		font-size: 20upx;
 	}
-	.text view{
-		color:#ef1c00;
+
+	.text view {
+		color: #ef1c00;
 	}
-	.content{
+
+	.content {
 		width: 100%;
 		height: 320upx;
 		overflow: auto;
 	}
-	.footer{
+
+	.footer {
 		width: 100%;
 		height: 100upx;
 		line-height: 100upx;
-		position: sticky;
-		bottom:0;
+		position: fixed;
+		bottom: 0;
 		background: #ef1c00;
-		text-align:center;
-		color:#fff;
+		text-align: center;
+		color: #fff;
 		font-size: 30upx;
 	}
-	.list{
+
+	.list {
 		height: 120upx;
 		width: 100%;
-		border-bottom:1px solid #ddd;
+		border-bottom: 2upx solid #ddd;
 	}
-	.select{
+
+	.select {
 		color: #000;
 		font-size: 10upx;
+		margin-top: 20upx;
 	}
-	.kind{
+
+	.kind {
 		width: 70upx;
 		height: 38upx;
 		background: #eee;
 		border-radius: 6upx;
-		text-align:center;
+		text-align: center;
 		line-height: 38upx;
-		color:#000;
-		float:left;
-		margin-right: 10upx;
+		color: #000;
+		float: left;
+		margin: 20upx 10upx 0 0;
 		font-size: 2upx;
 	}
-	.buy{
+
+	.buy {
 		height: 80upx;
-		border-bottom:1px solid #ddd;
+		border-bottom: 1px solid #ddd;
 	}
-	.btn{
-		float:right;
+
+	.btn {
+		float: right;
+	}
+
+	.reduce {
+		color: #989898;
+	}
+
+	.add {
+		color: #666;
+	}
+
+	.selected {
+		color: #ee1d00;
+		background: #fff;
+		border: 2upx solid #ee1d00;
 	}
 </style>
